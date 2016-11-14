71741ee2e87de55f38bacf363f5ce8b493f5a517
777Max777 <maximus-lihotin@mail.ru>
2016-11-14 23:54:50 +0200

    Тестовый проект для работы с картами

---
+++ testBrowse/testBrowse/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Web;
+
+
+namespace testBrowse
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //root is a grid element identified in the XAML
+      
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+             
+        private void webb_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            string curDir = Directory.GetCurrentDirectory();
+            try
+            {
+                this.webb.Navigate(new Uri(String.Format("file:///{0}/test.html", curDir)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Search_data_Click(object sender, RoutedEventArgs e)
+        {
+            /*string street = txt_Street.Text;
+            string city = txt_City.Text;
+            string state = txt_State.Text;
+            string zip = txt_Zip.Text;
+
+            try
+            {
+                StringBuilder queryaddres = new StringBuilder();
+                queryaddres.Append("http://maps.google.com/maps?q=");   
+                if(street != string.Empty)
+                {
+                    queryaddres.Append(street + "," + "+");
+                }
+                if (city != string.Empty)
+                {
+                    queryaddres.Append(city + "," + "+");
+                }
+                if (state != string.Empty)
+                {
+                    queryaddres.Append(state + "," + "+");
+                }
+                if (zip != string.Empty)
+                {
+                    queryaddres.Append(zip + "," + "+");
+                }
+                webb.Navigate(queryaddres.ToString());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }*/
+        }
+    }
+}
---
+++ testBrowse/testBrowse/mapbasics.js
@@ -0,0 +1,47 @@
+﻿/*var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init() {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [55.76, 37.64], // Москва
+        zoom: 10,
+        click: function (e) {
+            alert('click');
+    },
+        searchControlProvider: 'yandex#search'
+    });
+}*/
+/*var myMap;
+function init()
+{
+    myMap = new GMaps({
+        el: '#map',
+        lat: -12.0433,
+        lng: -77.0283,
+        zoom: 12
+    });
+}*/
+var map;
+
+function initMap() {
+    var myLatlng = new google.maps.LatLng(55.76, 37.64);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatlng,
+        zoom: 12
+    });
+
+    var marker = new google.maps.Marker({
+    position: myLatlng,
+    map:map,
+    title: "Hello World!"   
+    });
+}
+
+//marker.setMap(map);
