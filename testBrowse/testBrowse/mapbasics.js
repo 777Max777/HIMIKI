153ca9bd25f4c9f70e7834001b59fbb82d49ca41
777Max777 <maximus-lihotin@mail.ru>
2016-11-17 19:56:41 +0200

    Сделал маркер по адресу

--- testBrowse/testBrowse/mapbasics.js
+++ testBrowse/testBrowse/mapbasics.js
@@ -1,47 +1,27 @@
-﻿/*var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
-
-function init() {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [55.76, 37.64], // Москва
-        zoom: 10,
-        click: function (e) {
-            alert('click');
-    },
-        searchControlProvider: 'yandex#search'
-    });
-}*/
-/*var myMap;
-function init()
-{
-    myMap = new GMaps({
-        el: '#map',
-        lat: -12.0433,
-        lng: -77.0283,
-        zoom: 12
-    });
-}*/
+﻿var geocoder;
 var map;
-
-function initMap() {
-    var myLatlng = new google.maps.LatLng(55.76, 37.64);
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatlng,
-        zoom: 12
-    });
-
-    var marker = new google.maps.Marker({
-    position: myLatlng,
-    map:map,
-    title: "Hello World!"   
-    });
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+        zoom: 8,
+        center: latlng
+    }
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    codeAddress();
 }
 
-//marker.setMap(map);
+function codeAddress() {
+    var address = document.getElementById("address").value;
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
