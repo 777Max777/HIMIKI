0711a3287ec18524851b7fba0bff32fce2acb6bc
777Max777 <maximus-lihotin@mail.ru>
2016-11-21 23:46:09 +0200

    Login Соединение с сервером И первый запрос. Выдаётся всё через жопу. Но дальше работать можно

---
+++ diplom/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+
+namespace diplom
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>   
+
+    public partial class Login : Window
+    {
+        public SqlConnection conn;
+
+        public Login()
+        {
+            InitializeComponent();
+            textBox.Text = "LIHOTIN";
+            textBox1.Text = "ENose";
+            textBox2.Text = "test";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (conn = new System.Data.SqlClient.SqlConnection("Server=" + textBox.Text + ";Database=" + textBox1.Text + ";uid=" + textBox2.Text + ";pwd=" + textBox3.Text + ""))
+            {
+                try
+                {
+                    conn.Open();
+                    MessageBox.Show("Соединено");
+                    Data.value = "Server=" + textBox.Text + ";Database=" + textBox1.Text + ";uid=" + textBox2.Text + ";pwd=" + textBox3.Text + "";
+                    this.Close();
+                }
+                catch(Exception help)
+                {
+                    MessageBox.Show(help.Message);
+                }
+            }
+        }
+    }
+}
--- diplom/MainWindow.xaml.cs
+++ diplom/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +15,43 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace diplom
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
+    static class Data
+    {
+        public static string value { get; set; }        
+    }
+    
+
     public partial class MainWindow : Window
     {
+        
+        private Login logwin; 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void updateTable()
+        {
+            using (DataTable dt = new DataTable())
+            {
+                using (SqlConnection conn = new SqlConnection(Data.value))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Type_water ORDER BY id_type ASC", conn))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+                dataGrid.ItemsSource = dt.DefaultView;
+                dataGrid.AutoGenerateColumns = true;
+                dataGrid.CanUserAddRows = false;
+            }
         }
 
         public void Tree_Categories_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -30,5 +59,15 @@ namespace diplom
 
         }
 
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            logwin = new Login();
+            logwin.ShowDialog();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            updateTable();
+        }
     }
 }
